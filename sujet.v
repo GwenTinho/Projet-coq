daa7492d4c8ad1eaa9d0a44aeb6976d42ddab79e
gwentinho <qschroed@gmail.com>
2024-04-16 12:38:16 +0200

    new attempt yey

--- sujet.v
+++ sujet.v
@@ -125,6 +125,8 @@ Inductive LI : sequent -> Set :=
     Γ ++ [(A ⇒ B) ⇒ C] ++ Γ' ⊢ D
 where "G ⊢ A" := (LI (G ⊢? A)).
 
+Hint Constructors LI : core.
+
 (* On peut montrer la terminaison en définissant une notion de degré
    pour les formules, puis pour les séquents : on montre ensuite que
    le degré décroit strictement entre la conclusion des règles et
@@ -146,6 +148,9 @@ Definition deg_context (l : list formula) : nat :=
 (* Idem pour un séquent. *)
 Definition deg_sequent '(Γ ⊢? A) : nat :=
   deg_context Γ + deg A.
+
+Hint Unfold deg_context : core.
+Hint Unfold deg_sequent : core.
 
 Require Import ZArith Psatz.
 
@@ -201,6 +206,39 @@ Proof.
   induction A; simpl; lia.
 Qed.
 
+Lemma deg_IE_bot : forall Γ, deg_sequent (Γ ⊢? ⊥) >= deg_sequent ([] ⊢? ⊥).
+Proof.
+  intros Γ.
+  simpl. lia.
+Qed.
+
+
+Lemma deg_II_or_left : forall Γ A B, deg_sequent (Γ ⊢? A) < deg_sequent (Γ ⊢? A ∨ B).
+Proof.
+  intros Γ A B.
+  specialize (deg_at_least_two A).
+  specialize (deg_at_least_two B).
+  intros H H0.
+  induction Γ.
+  - simpl. lia.
+  - unfold deg_sequent, deg_context in *. simpl in *.
+    lia.
+Qed.
+
+
+Lemma deg_II_or_right : forall Γ A B, deg_sequent (Γ ⊢? B) < deg_sequent (Γ ⊢? A ∨ B).
+Proof.
+  intros Γ A B.
+  specialize (deg_at_least_two A).
+  specialize (deg_at_least_two B).
+  intros H H0.
+  induction Γ.
+  - simpl. lia.
+  - unfold deg_sequent, deg_context in *. simpl in *.
+    lia.
+Qed.
+
+
 (* On prouvera que les prémisses des règles ont des degrés plus petits
    que les conclusions. *)
 Lemma deg_IE_and :
@@ -213,9 +251,9 @@ Proof.
     induction  Γ1.
     + simpl.
       apply Nat.add_lt_mono_r.
-      unfold deg_context.
       specialize (deg_and_sum A B) as G.
       simpl in G.
+      unfold deg_context.
       simpl.
       lia.
     + apply Nat.add_lt_mono_r.
@@ -232,6 +270,33 @@ Proof.
     simpl in IHΓ0.
     lia.
 Qed.
+
+Lemma deg_II_and_left :  forall  Γ A B,
+    deg_sequent (Γ  ⊢? A) < deg_sequent (Γ ⊢? A ∧ B).
+Proof.
+  intros Γ A B.
+  induction Γ.
+  - simpl. specialize (deg_at_least_two B). specialize (deg_at_least_two A). nia.
+  - unfold deg_sequent, deg_context in *. simpl in *.
+    specialize (deg_at_least_two A).
+    specialize (deg_at_least_two B).
+    specialize (deg_at_least_two a).
+    nia.
+Qed.
+
+Lemma deg_II_and_right :   forall  Γ A B,
+    deg_sequent (Γ  ⊢? B) < deg_sequent (Γ ⊢? A ∧ B).
+Proof.
+  intros Γ A B.
+  induction Γ.
+  - simpl. specialize (deg_at_least_two B). specialize (deg_at_least_two A). nia.
+  - unfold deg_sequent, deg_context in *. simpl in *.
+    specialize (deg_at_least_two A).
+    specialize (deg_at_least_two B).
+    specialize (deg_at_least_two a).
+    nia.
+Qed.
+
 
 Lemma deg_IE_or_left :
   forall Γ0 A B Γ1 C,
@@ -549,8 +614,6 @@ Proof.
       * intros A0 B M [L|R].
         -- contradiction.
         -- assumption.
-           
-        
 Defined.
 Check in_split_specif.
 Definition in_split_formula {x l} := @in_split_specif formula formula_eq_dec x l.
@@ -638,14 +701,18 @@ Admitted.
    section où l’on fait cette hypothèse. À l’issue de cette section,
    nous terminons la preuve en appliquant les lemmes par induction. *)
 
-Lemma list_factor_empty : forall {A} (l0 l1 : list A) (a : A), l0 ++ a :: l1 <> [].
+Lemma list_factor_empty : forall {A}  (l0 l1 : list A) (a : A), l0 ++ a :: l1 <> [].
 Proof.
   intros A l0 l1 a Neg.
   induction l0; inversion Neg.
 Qed.
 
+Ltac kill_empty := match goal with
+                       | H : (?l0 ++ ?a :: ?l1 = []) |-  _  => (apply list_factor_empty in H; contradiction)
+                   end.
 
-  Lemma increasing_sequent_degree : forall B Γ A, deg_sequent (Γ ⊢? A) < deg_sequent ((B :: Γ) ⊢? A).
+
+ Lemma increasing_sequent_degree : forall B Γ A, deg_sequent (Γ ⊢? A) < deg_sequent ((B :: Γ) ⊢? A).
   Proof.
     intro B.
     simpl.
@@ -681,10 +748,11 @@ Qed.
 Admitted.
 
 
+ 
 
 
-  
-Lemma list_split_ind:
+
+ Lemma list_split_ind:
   forall A P Q (l : list A)
   (f : forall l0 x l1, l0 ++ [x] ++ l1 = l -> P + { Q l0 x l1 }),
   P + { forall l0 x l1, l0 ++ [x] ++ l1 = l -> Q l0 x l1 }.
@@ -698,21 +766,85 @@ Proof. (* Defined plutôt que Qed *)
     right.
     intros l0 x l1 H0.
     simpl in H0.
-      apply list_factor_empty in H0.
-      contradiction.
+      kill_empty.
   - intros Q post acc H.
     (*Idea: if i find the element within l -> ok, if i dont check if a is the element*)
     admit.
 Admitted.
-     
+
+Lemma zero_context_is_empty : forall Γ, deg_context Γ = 0 -> Γ = [].
+Proof.
+  intros Γ H.
+  induction Γ.
+  - reflexivity.
+  - unfold deg_context in H. simpl in H. specialize (deg_at_least_two a). lia.
+Qed.
+  
+
+ (*IDEA: implement lemmas from the paper itself*)
+
+  (*notably: most rules of LG are invertible*)
+
+Example from_text :  forall A C D E F, [(A ∨ (D ∧ (E ⇒  (C ∨ D)))) ⇒ D] ⊢ (A ⇒ (D ∧ (E ⇒ (C ∨ D)))) ∨ F.
+Proof.
+  intros A C D E F.
+  apply II_or_left.
+  apply II_impl.
+  specialize (@IE_impl_or [] [A]) as H.
+  apply H.
+  simpl.
+  clear H.
+  apply II_and.
+  - apply @IE_impl_and with (Γ := [A ⇒ D]) (Γ' := [A]).
+    apply @IE_impl_right with (Γ := []).
+    apply @IE_impl_left with (Γ := []) (Γ' := []).
+    simpl.
+    apply @I_ax with (Γ := []) (Γ' := [(E ⇒ C ∨ D) ⇒ D; A]).
+  - apply II_impl.
+    apply II_or_right.
+    apply @IE_impl_and with (Γ := [A ⇒ D]) (Γ' := [A;E]).
+    apply @IE_impl_right with (Γ := []) (Γ'' := [E]).
+    simpl.
+    apply @IE_impl_left with (Γ := []) (Γ' := []) (Γ'' := [A;E] ).
+    simpl.
+    apply @I_ax with (Γ := []).
+Qed.
+
+Lemma deg_context_geq_O : forall Γ, deg_context Γ >= 0.
+Proof.
+  unfold deg_context.
+  induction Γ.
+  - lia.
+  - specialize (deg_at_least_two a).
+    intro H. simpl. lia.
+Qed.
+
+Lemma deg_sequent_geq_two : forall Γ A, deg_sequent (Γ ⊢? A) >= 2.
+Proof.
+  intros Γ A.
+  simpl.
+  specialize (deg_context_geq_O Γ).
+  specialize (deg_at_least_two A).
+  intros H H0.
+  lia.
+Qed.
+  
+
 Lemma var_characteristic : forall Γ x,  (Γ ⊢ Var x) -> In (Var x) Γ.
 Proof.
-  intros Γ x H.
+  intros Γ x.
+  
   induction Γ.
-  - inversion H; ( (apply list_factor_empty in H1) + (apply list_factor_empty in H0)); contradiction.
-  - simpl.
-    admit.
+  - intro H. inversion H; kill_empty.
+  - intro H.
+    destruct (formula_eq_dec a (Var x)) as [L | R].
+    + simpl. left. assumption.
+    + simpl. right.
+
 Admitted.
+    
+
+    
     
     
 
@@ -755,37 +887,112 @@ Section LI_Decidable.
           admit.
         * assumption.
       + admit.
-   Admitted.
-
-      
-  Lemma is_provable_rec_var : forall x,  A = Var x -> (Γ ⊢ Var x) + {notT (Γ ⊢ Var x)}.
-  Proof.
-    intros x eq.
-    rewrite eq in *.
-   specialize (in_dec_formula (Var x) Γ) as [InΓ | NInΓ].
-    + left.
-      Check in_split_formula.
-        specialize (in_split_formula InΓ) as [[Before After] H].
-        rewrite H.
-        apply I_ax.
-      + right.
-        intro H.
-        apply NInΓ.
-        admit.
-Admitted.
-      
+  Admitted.
+                 
   
   Lemma is_provable_rec :
     (Γ ⊢ A) + { notT (Γ ⊢ A) }.
   Proof.
-    induction A.
-    - left. apply is_provable_rec_top.
-    - (* apply is_provable_rec_bot.*) admit.
-    - (* apply ...*) admit.
-    - admit.
-    - admit.
-    - admit. 
+    remember (deg_sequent (Γ ⊢? A)) as n.
+    induction n.
+    - specialize (deg_sequent_geq_two Γ A) as Absurd. lia.
+    - induction A.
+      + left. apply II_top.
+      + specialize (deg_IE_bot Γ) as Geq.
+        rewrite <- Heqn in Geq.
+        specialize (Nat.eq_dec (Datatypes.S n) (deg_sequent ([] ⊢? ⊥))) as [L | R].
+        * simpl in L. rewrite Heqn in L. simpl in L.
+          cut (deg_context Γ = 0).
+          -- intro H.
+             apply zero_context_is_empty in H.
+             rewrite H in *.
+             clear H L.
+             right.
+             intro K.
+             inversion K; kill_empty.
+          -- lia.
+        * Search (?a <= ?b /\ ?a <> ?b).
+          cut (deg_sequent ([] ⊢? ⊥) <= Datatypes.S n /\  deg_sequent ([] ⊢? ⊥) <> Datatypes.S n).
+          -- intro Hyp.
+             specialize (proj2 (Nat.le_neq (deg_sequent ([] ⊢? ⊥)) (Datatypes.S n)) Hyp) as K.
+             assert (J : deg_sequent ([] ⊢? ⊥) < Datatypes.S n). assumption.
+             apply IH in K.
+             destruct K as [LL | RR].
+             ++  inversion LL; kill_empty.
+             ++ simpl in J.
+                left.
+                induction Γ.
+                ** rewrite Heqn in J.
+                  simpl in J.
+                  lia.
+                ** admit.
+          -- admit.
+      + admit.
+      + specialize (deg_II_and_left Γ f1 f2) as F1.
+        specialize (deg_II_and_right Γ f1 f2) as F2.
+        rewrite <- Heqn in F1,F2.
+        apply IH in F1,F2.
+        destruct F1 as [F1 | NF1];
+          destruct F2 as [F2| NF2].
+        * left. apply II_and; assumption.
+        * right.
+          intro K.    
+          inversion K.
+          -- admit.
+          -- apply NF2.
+             rewrite <- H0.
+             apply IE_bot.
+          -- contradiction.
+          -- admit.
+          -- admit.
+          -- admit.
+          -- admit.
+          -- admit.
+          -- admit.
+          -- admit.
+          -- admit.
+        * admit.
+        * admit.
+      + specialize (deg_II_or_left Γ f1 f2) as F1.
+        specialize (deg_II_or_right Γ f1 f2) as F2.
+        rewrite <- Heqn in F1,F2.
+        apply IH in F1,F2.
+        destruct F1 as [F1 | NF1]; destruct F2 as [F2 | NF2].
+        * left. apply II_or_left. assumption.
+        * left. apply II_or_left. assumption.
+        * left. apply II_or_right. assumption.
+        * right. intro K.
+          inversion K.
+          -- specialize (Nat.eq_dec (Datatypes.S n) (deg_sequent (Γ ⊢? f1))) as [L | R].
+             ++ apply IHf1 in L.
+                ** admit.
+                ** admit.
+             ++ admit.
+          -- apply NF1.
+             rewrite <- H0.
+             apply IE_bot.
+          -- admit.
+          -- admit.
+          -- admit.
+          -- admit.
+          -- admit.
+          -- admit.
+          -- admit.
+          -- admit.
+          -- admit.
+          -- admit.
+      + specialize (deg_II_impl Γ f1 f2) as F.
+        rewrite <- Heqn in F.
+        apply IH in F.
+        destruct F as [F | NF].
+        * left. apply II_impl. assumption.
+        * admit.
+Admitted.
 
+             
+           
+        
+          
         
         
       
@@ -804,10 +1011,9 @@ Admitted.
     For each already proven thing
     if match p with II and  _ _ _ _ => False, else True
     Simplifies refuation!
-    Read proofs and types
 
    *)
-  Admitted. (* Defined. *)
+   (* Defined. *)
 End LI_Decidable.
 
 (* On peut facilement déduire is_provable de is_provable_rec par
